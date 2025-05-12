ab1c46492b1e87b4636dd12c69064499fd296e60
hameedaah <abhameedat@gmail.com>
2025-05-12 20:07:04 +0100

    Added file to git

---
+++ main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+books = [
+    {"title": "Book 1", "author": "Author 1"},
+    {"title": "Book 2", "author": "Author 2"},
+    {"title": "Book 3", "author": "Author 3"},
+    {"title": "Book 4", "author": "Author 4"},
+    {"title": "Book 5", "author": "Author 5"},
+    {"title": "Book 6", "author": "Author 6"},
+    {"title": "Book 7", "author": "Author 7"},
+    {"title": "Book 8", "author": "Author 8"},
+    {"title": "Book 9", "author": "Author 9"},
+    {"title": "Book 10", "author": "Author 10"},
+]
+
+#ALL FUNCTIONS
+
+def get_all_books():
+    return books
+
+
+def get_book_by_title(book_title):
+    new_books = []
+    for book in books:
+        if book_title.lower() == book["title"].lower():
+            new_books.append(book)
+    return new_books
+
+def post_book_by_author(author_name):
+    new_books = []
+    for book in books:
+        if author_name.lower() == book["author"].lower():
+            new_books.append(book)
+    return new_books
+
+
+def add_new_book_to_list(title: str, author: str):
+    for existing_book in books:
+        if existing_book["title"].lower() == title.lower() and existing_book["author"].lower() == author.lower():
+            return "Error: This book already exists."
+
+    books.append({"title": title, "author": author})
+    return "Success"
+
+def delete_book_by_title(book_title):
+    for book in books:
+        if book_title.lower() == book["title"].lower():
+            books.remove(book)
+
+    return f"Success: {book['title']} has been removed"
+
+
+#ALL ENDPOINTS
+@app.get("/")
+def read_root():
+    return {"Python is the best programming language!"}
+
+
+@app.get("/all-books")
+def all_books():
+    response = get_all_books()
+    return response
+
+
+@app.post("/get-book-by-name")
+def book_by_name(request:dict):
+    book_name = request["book_name"]
+    response = get_book_by_title(book_name)
+    return response
+
+@app.post("/get-book-by-author")
+def book_by_author(request: dict):
+    author = request["author"]
+    response = post_book_by_author(author)
+    return response
+
+@app.post("/add-new-book")
+def add_book(request: dict):
+    title = request["title"]
+    author = request["author"]
+    result = add_new_book_to_list(title, author)
+
+    if result.startswith("Error"):
+        return {"error": result}
+    return {"message": result}
+
+@app.delete("/delete-book-by-title")
+def del_book(request:dict):
+    book_name = request["book_name"]
+    response = delete_book_by_title(book_name)
+    return response
+
+
+#print(get_book_by_title("book 1"))
